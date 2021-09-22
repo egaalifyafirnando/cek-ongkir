a67b5afd9b7a09a1fb2c283f7af5b48031196077
egaalfy <egaalifyaf@gmail.com>
2021-09-23 01:51:01 +0700

    update url app.php"

--- config/app.php
+++ config/app.php
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', '
+    https://cek-ongkir-app.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
